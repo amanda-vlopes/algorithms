b0bae99bccbca6ddb34f3836379ca08516916e4d
amanda-vlopes <amandavitor@hotmail.com>
2023-10-23 19:42:53 -0300

    Requisito 1 finalizado

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,17 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui  ."""
-    raise NotImplementedError
+
+    if target_time is None:
+        return None
+
+    students = 0
+
+    for hours in permanence_period:
+        if not (isinstance(hours[0], int) and isinstance(hours[1], int)):
+            return None
+        if target_time >= hours[0] and target_time <= hours[1]:
+            students += 1
+    return students
+
+
+exemplo = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+print(study_schedule(exemplo, 5))
