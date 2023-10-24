e844fddb9b57f41603657e99c05928168dd0cc8a
amanda-vlopes <amandavitor@hotmail.com>
2023-10-24 15:37:40 -0300

    Requisito 3 finalizado

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -11,7 +11,3 @@ def study_schedule(permanence_period, target_time):
         if target_time >= hours[0] and target_time <= hours[1]:
             students += 1
     return students
-
-
-exemplo = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
-print(study_schedule(exemplo, 5))
