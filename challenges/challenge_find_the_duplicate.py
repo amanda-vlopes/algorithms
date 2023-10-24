ed98ead37f2a63205f4c67e05d779d5d3c356a52
amanda-vlopes <amandavitor@hotmail.com>
2023-10-24 20:47:07 -0300

    Requisito 5 finalizado

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,13 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    nums.sort()
+
+    for index in range(0, len(nums) - 1):
+        if type(nums[index]) is str or nums[index] < 0:
+            return False
+        if nums[index] == nums[index + 1]:
+            return nums[index]
+
+    return False
+
+
+print(find_duplicate([1, 2, 2, 3, 4]))
