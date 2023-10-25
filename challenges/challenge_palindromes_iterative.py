d9c25c2fa20cc21b10713a4d84513190dbe0e8ff
amanda-vlopes <amandavitor@hotmail.com>
2023-10-25 16:20:31 -0300

    fix error

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,11 +1,8 @@
 def is_palindrome_iterative(word):
-    if word == "":
+    if len(word) == 0:
         return False
 
     for index in range(0, len(word) - 1):
         if word[index] != word[len(word) - 1 - index]:
             return False
     return True
-
-
-print(is_palindrome_iterative(""))
--- challenges/tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-    if word == "":
-        return False
